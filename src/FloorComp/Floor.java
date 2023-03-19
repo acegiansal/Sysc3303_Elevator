19dbb05910c209643c419e013a6dcf5f03301c4b
robbiekuhn <robbie.kuhn@carleton.ca>
2023-03-19 16:50:51 -0400

    updating new floor work

--- src/FloorComp/Floor.java
+++ src/FloorComp/Floor.java
@@ -1,10 +1,13 @@
 package FloorComp;
 
+import ElevatorComp.ElevatorCar;
+
 public class Floor {
 
     int floorNum;
     FloorButton floorButton;
-    DirectionLamp directionLamp;
+    DirectionLamp upLamp;
+    DirectionLamp downLamp;
     FloorSend.floorType floorType;
 
 
@@ -14,14 +17,24 @@ public class Floor {
         this.floorNum = floorNum;
         this.floorType = type;
         this.floorButton = new FloorButton("Up");
-        this.directionLamp = new DirectionLamp();
+        this.upLamp = new DirectionLamp();
+        this.downLamp = new DirectionLamp();
     }
 
     public void send(String direction){
         this.floorButton.pressed(direction);
-        this.directionLamp.toggleLamp(true);
+//        this.directionLamp.toggleLamp(true);
     }
 
-
-
+    public void arrive(String direction, int elevatorID){
+        if (direction.equals("Up")){
+            upLamp.toggleLamp(true);
+        } else if (direction.equals("Down")) {
+            downLamp.toggleLamp(true);
+        } else {
+            upLamp.toggleLamp(false);
+            downLamp.toggleLamp(false);
+        }
+        // LOGGER PRINT THE ELEVATOR ARRIVING AT THIS FLOOR
+    }
 }
